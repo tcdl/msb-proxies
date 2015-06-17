f1f8a0e0c71bd3d636dc31363b1f2605520a42b7
Pascal Laenen <pascal@Pascals-MacBook-Pro.local>
2015-06-17 15:04:47 +0100

    solved bug, normaliseQuery was not working as expected

--- test/http2bus.normaliseQuery.test.js
+++ test/http2bus.normaliseQuery.test.js
@@ -12,51 +12,27 @@ var afterEach = lab.afterEach;
 var expect = Code.expect;
 
 /* Modules */
-var queryParamToObject = require('./../lib/http2bus/middleware/normaliseQuery');
+var normaliseQuery = require('./../lib/http2bus/middleware/normaliseQuery');
 
 /* Tests */
 describe('http2bus.normaliseQuery', function() {
 
   it('it should be possible to parse convert query strings to objects', function(done) {
     var req = {
-      query : {
-        a : 'a',
-        'b.c' : 'b_c'
-      }
+      url : 'http://127.0.0.1?a=a&b.c=b_c'
     };
-    queryParamToObject(req, null, function() {
-      expect(req.query).to.be.deep.equal({ a: 'a', b: { c: 'b_c' } });
-      done();
-    });
-  });
 
-  it('it should be possible to parse convert query strings to objects and keep existing objects', function(done) {
-    var req = {
-      query : {
-        a : 'a',
-        'b.c' : 'b_c',
-        d : {
-          e : 'e'
-        }
-      }
-    };
-    queryParamToObject(req, null, function() {
-      expect(req.query).to.be.deep.equal({ a: 'a', b: { c: 'b_c' }, d: { e: 'e' } });
+    normaliseQuery(req, null, function() {
+      expect(req.query).to.be.deep.equal({ a: 'a', b: { c: 'b_c' } });
       done();
     });
   });
 
   it('it should be possible to parse convert query strings to array', function(done) {
     var req = {
-      query : {
-        a : 'a',
-        'b[0]' : 'b',
-        d : {
-          e : 'e'
-        }
-      }
+      url : 'http://127.0.0.1?a=a&b[0]=b&d.e=e'
     };
-    queryParamToObject(req, null, function() {
+    normaliseQuery(req, null, function() {
       expect(req.query).to.be.deep.equal({ a: 'a', b: ['b'], d: { e: 'e' } });
       done();
     });
